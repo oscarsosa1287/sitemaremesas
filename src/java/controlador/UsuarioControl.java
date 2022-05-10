9f8be0e8d8062b394c5a11eb7d4779741cb95d4e
Oscar Sosa <oscar.sosa@creativaconsultores.com>
2022-05-09 21:49:41 -0600

    Mantenimiento Usuario

---
+++ src/java/controlador/UsuarioControl.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import modelo.dao.UsuarioDao;
+import modelo.entidad.Usuario;
+
+/**
+ *
+ * @author Oscar Sosa
+ */
+//@Named(value = "usuarioControl")
+//@Dependent
+@ManagedBean
+@ViewScoped
+public class UsuarioControl implements Serializable {
+
+    private List<Usuario> listaUsuarios;
+    private Usuario usuario;
+
+    /**
+     * Creates a new instance of UsuarioControl
+     */
+    public UsuarioControl() {
+    }
+
+    public List<Usuario> getListaUsuarios() {
+        UsuarioDao ed = new UsuarioDao();
+        listaUsuarios = ed.listarEstudtantes();
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void limpiarUsuario() {
+        usuario = new Usuario();
+    }
+
+    public void agregarUsuario() {
+        UsuarioDao ed = new UsuarioDao();       
+        ed.agregar(usuario);
+    }
+
+    public void modificarUsuario() {
+        UsuarioDao ed = new UsuarioDao();
+        ed.modificar(usuario);
+        limpiarUsuario();
+    }
+
+    public void eliminarUsuario() {
+        UsuarioDao ed = new UsuarioDao();
+        ed.eliminar(usuario);
+        limpiarUsuario();
+
+    }
+}
+
---
+++ src/java/modelo/dao/UsuarioDao.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import java.util.List;
+import modelo.entidad.Usuario;
+import modelo.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Oscar Sosa
+ */
+public class UsuarioDao {
+
+    public List<Usuario> listarEstudtantes() {
+        List<Usuario> lista = null;
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sesion.beginTransaction();
+
+        String hql = "FROM Usuario";
+        try {
+            lista = sesion.createQuery(hql).list();
+            t.commit();
+            sesion.close();
+        } catch (Exception e) {
+            t.rollback();
+        }
+        return lista;
+    }
+
+    public void agregar(Usuario usuario) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(usuario);
+            sesion.getTransaction().commit();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void modificar(Usuario usuario) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(usuario);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void eliminar(Usuario usuario) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(usuario);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+}
+
